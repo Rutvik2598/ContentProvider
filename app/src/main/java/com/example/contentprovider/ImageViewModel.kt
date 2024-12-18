334248d8d538b4c199a0dc6ba8dfe9847bc25a06
Rutvik Vishwas Kulkarni <kulkarnirutvikvishwas@gmail.com>
2024-12-18 15:28:49 -0500

    Content resolver and provider

---
+++ app/src/main/java/com/example/contentprovider/ImageViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.contentprovider
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel: ViewModel() {
+
+    var images by mutableStateOf(emptyList<Image>())
+        private set
+
+    fun updateImages(images: List<Image>) {
+        this.images = images
+    }
+
+}
